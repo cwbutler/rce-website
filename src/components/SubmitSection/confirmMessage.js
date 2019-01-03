ac98c24e79809b7be786b810b6786b93ced55547
cbutler <cbutler@babelstreet.com>
2019-01-03 12:43:55 -0500

    Removed redundant thank you text

--- src/components/SubmitSection/confirmMessage.js
+++ src/components/SubmitSection/confirmMessage.js
@@ -3,8 +3,6 @@ import React from 'react';
 export default function ConfirmMessage() {
   return (
     <div className="confirm primary-color">
-      <h4>Thank You!</h4>
-
       <p>Thank you for your interest in helping <br /> RCE create a world with fewer heart attacks.</p>
 
       <p>We will be in contact soon.</p>
